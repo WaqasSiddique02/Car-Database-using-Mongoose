8255d9fef697a74b8aeb0f7c3dfa43429ab45a8e
WaqasSiddique02 <siddique02mwaqas@gmail.com>
2024-07-09 15:23:43 +0500

    complete mongoose

--- app.js
+++ app.js
@@ -17,40 +17,40 @@ const carSchema=new Schema({
 
 const Car =mongoose.model("Car",carSchema);
 
-// const car =new Car({
-//     name:"Mehran",
-//     rating: 4,
-//     review: "Good car"
-// });
+const car =new Car({
+    name:"Mehran",
+    rating: 4,
+    review: "Good car"
+});
 
-// const corolla =new Car ({
-//     name:"Corolla",
-//     rating: 4.5,
-//     review: "Good car"
-// });
+const corolla =new Car ({
+    name:"Corolla",
+    rating: 4.5,
+    review: "Good car"
+});
 
-// const h2r=new Car({
-//     name:"H2R",
-//     rating: 5,
-//     review: "Good Bike"
-// });
+const h2r=new Car({
+    name:"H2R",
+    rating: 5,
+    review: "Good Bike"
+});
 
-// Car.insertMany([corolla, h2r])
-//   .then(function(docs) {
-//     console.log("Data inserted successfully in carsDB");
-//   })
-//   .catch(function(err) {
-//     console.error(err);
-//   });
+Car.insertMany([corolla, h2r])
+  .then(function(docs) {
+    console.log("Data inserted successfully in carsDB");
+  })
+  .catch(function(err) {
+    console.error(err);
+  });
 
 
-// Car.find({})
-// .then(function(docs) {
-//     console.log(docs);
-// })
-// .catch(function(err){
-//     console.error(err);
-// });
+Car.find({})
+.then(function(docs) {
+    console.log(docs);
+})
+.catch(function(err){
+    console.error(err);
+});
 
 Car.find({})
 .then(function (docs) {
